455d344fd2173beae4d203bd12425bcc07a7a1c2
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2026-08-23 16:51:04 +0100

    test: update widget tests and verify clean test and analysis

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,17 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:trueface_liveness_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
+  testWidgets('Verify Liveness Example App loads', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) =>
-            widget is Text && widget.data!.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.text('Create Session & Start Check'), findsOneWidget);
   });
 }
